9d9a14828a38d205ffb992a918d51053a1e35b8b
Oleg Sokolov <sokolegg@yandex.ru>
2020-01-01 22:25:58 -0800

    dqn gan

--- dqn_env/envs/dqn_env.py
+++ dqn_env/envs/dqn_env.py
@@ -1,18 +1,22 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import numpy as np
 
 class DQNEnv(gym.Env):
   metadata = {'render.modes': ['human']}
   
   def __init__(self, reward_f):
     self.reward_f = reward_f
+    self.color_size = 2
+    self.reset()
     
   def step(self, action):
-    pass
+    self.image[self.position] = action
     
   def reset(self):
-    pass
+    self.image = np.random.randn(28, 28, 1)
+    self.position = np.array([0, 0])
     
   def render(self, mode='human', close=False):
     pass
